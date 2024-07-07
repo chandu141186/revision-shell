8374379091afd81e8dfecdd30e527463c2704b1a
chandu141186 <chandu141186@gmail.com>
2024-07-07 22:50:16 +0530

    variable commit

--- shell-scripting1/Variable.sh
+++ shell-scripting1/Variable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ramesh give works to suresh
-suresh get work  from ramesh
-ramesh allocate github work to suresh
-suresh ask some doubt on work assined by ramesh
+echo "ramesh: Give works to suresh"
+echo "suresh: get work  from ramesh"
+echo "ramesh: allocate github work to suresh"
+echo "suresh: ask some doubt on work assined by ramesh"
