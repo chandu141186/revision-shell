c1eda98972c777097d5c4c1b16d1e5e90be3f5d3
chandu141186 <chandu141186@gmail.com>
2024-07-07 23:32:53 +0530

    variable commit

--- shell-scripting1/Variable.sh
+++ shell-scripting1/Variable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PERSON1 = ramesh
-#PERSON2 = suresh
-echo " $PERSON1:: Give works to $PERSON2 "
-echo " $PERSON2:: Get work  from $PERSON1 "
-echo " $PERSON1:: Allocate github work to $PERSON2 "
-echo " $PERSON2:: Ask some doubt on work assined by $PERSON1 "
+PERSON1 = ramesh
+PERSON2 = suresh
+echo "$PERSON1: Give works to $PERSON2 "
+echo "$PERSON2: Get work  from $PERSON1 "
+echo "$PERSON1: Allocate github work to $PERSON2 "
+echo "$PERSON2: Ask some doubt on work assined by $PERSON1 "
