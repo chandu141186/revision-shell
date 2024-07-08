db66f9b54437436f82f1996499bc0a86bf8ebb90
chandu141186 <chandu141186@gmail.com>
2024-07-08 14:39:02 +0530

    variable commit

--- shell-scripting1/install-mysql.sh
+++ shell-scripting1/install-mysql.sh
@@ -3,9 +3,9 @@
 ID=$(id -u)
 
 if ($ID -ne 0)
- echo "ERROR:: Please run this script with root access"
+ then echo "ERROR:: Please run this script with root access"
     exit 1 # you can give other than 0
- else
+else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
