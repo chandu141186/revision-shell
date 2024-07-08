c91dbaaa386c32b91d4f4088688e5b981ed02609
chandu141186 <chandu141186@gmail.com>
2024-07-08 14:37:57 +0530

    variable commit

--- shell-scripting1/install-mysql.sh
+++ shell-scripting1/install-mysql.sh
@@ -5,7 +5,7 @@ ID=$(id -u)
 if ($ID -ne 0)
  echo "ERROR:: Please run this script with root access"
     exit 1 # you can give other than 0
-else
+ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
@@ -15,7 +15,7 @@ if [ $? -ne 0 ]
 then
     echo "ERROR:: Installing MySQL is failed"
     exit 1
-else
+ else
     echo "Installing MySQL is SUCCESS"
 fi
 
@@ -25,6 +25,6 @@ if [ $? -ne 0 ]
 then
     echo "ERROR:: Installing GIT is failed"
     exit 1
-else
+ else
     echo "Installing GIT is SUCCESS"
 fi
