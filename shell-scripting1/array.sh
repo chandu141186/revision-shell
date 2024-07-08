0f8472e05926abdcfba60d6601b5532bc9b21f52
chandu141186 <chandu141186@gmail.com>
2024-07-08 10:56:36 +0530

    variable commit

--- shell-scripting1/array.sh
+++ shell-scripting1/array.sh
@@ -4,13 +4,13 @@ NAMES=("RAM" "BAM" "SHAM")
 
 echo "First name:: ${NAMES[0]}"
 
-echo "Second name:: ${NAMES(1)}"
+echo "Second name:: ${NAMES[1]}"
 
 
-echo "THIRD name:: $NAMES(2)"
+echo "THIRD name:: ${NAMES[2]}"
 
 
-echo "ALL name:: $NAMES(@)"
+echo "ALL name:: ${NAMES[@]}"
 
 
 
