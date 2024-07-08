eae845dbc41a5a69b414eb4cce1ea0b835bc7a1b
chandu141186 <chandu141186@gmail.com>
2024-07-08 09:32:25 +0530

    variable commit

--- shell-scripting1/variablep.sh
+++ shell-scripting1/variablep.sh
@@ -5,6 +5,6 @@ echo "Please enter your username:: "
 read  USERNAME #the value entered above will be automatically atteched to USERNAME variable
 
 echo "Please enter your Password:: "
-read  PASSWORD
+read -s PASSWORD
 
 echo "Username is: $USERNAME, Password is: $PASSWORD" # I am priting just for validation, you should not print username and passwords in scripts
