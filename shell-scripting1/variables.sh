7146a2b53bbb97a57426c0e570486b5ee712a93a
chandu141186 <chandu141186@gmail.com>
2024-07-08 09:12:31 +0530

    variable commit

---
+++ shell-scripting1/variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Give works to $PERSON2"
+echo "$PERSON2: Get work  from $PERSON1"
+echo "$PERSON1: Allocate github work to $PERSON2"
+echo "$PERSON2: Ask some doubt on work assined by $PERSON1"
