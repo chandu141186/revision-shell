955026e78bc68d92a18488fef86db94bf98cbcd9
chandu141186 <chandu141186@gmail.com>
2024-07-08 15:06:10 +0530

    variable commit

---
+++ shell-scripting1/logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(DATE+%F-%H-%M-%s)
+LOG_FILE=/tmp/$0-$TIMESTAMP
+
+VALIDATE(){
+
+   if [ $1 -ne 0 ]
+ then
+    echo "ERROR:: $2 is failed"
+    exit 1
+ else
+    echo "  $2 is SUCCESS"
+fi
+
+}
+
+if [ $ID -ne 0 ]
+ then echo "ERROR:: Please run this script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+yum install mysql -y
+
+ VALIDATE $? "Installing Mysql"
+
+yum install git -y
+
+  VALIDATE $? "Installing Git"
