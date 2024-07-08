c626cbccfeece531229cad7a369e0a511848d659
chandu141186 <chandu141186@gmail.com>
2024-07-08 15:40:48 +0530

    variable commit

--- shell-scripting1/logs.sh
+++ shell-scripting1/logs.sh
@@ -4,17 +4,17 @@ ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOG_FILE=/tmp/$0-$TIMESTAMP
 R="\e[31m"
-G="\e(32m"
+G="\e[32m"
 N="\e[0m"
 
 VALIDATE(){
 
    if [ $1 -ne 0 ]
  then
-    echo "ERROR:: $2 is $R failed $N"
+    echo -e "ERROR:: $2 is $R failed $N"
     exit 1
  else
-    echo "  $2 is $G SUCCESS $N"
+    echo -e "  $2 is $G SUCCESS $N"
 fi
 
 }
